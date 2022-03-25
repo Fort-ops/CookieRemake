a7cbe398b608ad0bf3ad32fa8bf581873f3947b9
Fort-ops <81819548+Fort-ops@users.noreply.github.com>
2022-03-25 10:26:50 -0400

    Update and rename CookieMonster.js to CookieMonster.user.js

--- CookieMonster.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    var checkReady = setInterval(function() {
-        if (typeof Game.ready !== 'undefined' && Game.ready) {
-            Game.LoadMod('https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js');
-            clearInterval(checkReady);
-        }
-    }, 1000);
-})();
